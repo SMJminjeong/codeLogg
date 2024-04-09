f1f225265c7c3f1c983d9e86c74078597a1ceac7
SMJminjeong <sdwp0920@gamil.com>
2024-04-09 13:03:11 +0900

    :art: add post

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -2,31 +2,72 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
+{/*<img src="img/diary.svg" alt="Diary"/>*/
+}
+{/*<img src="img/book.svg" alt="Diary"/>*/
+}
+{/*<img src="img/folder.svg" alt="Diary"/>*/
+}
+{/*<img src="img/rabbot.svg" alt="Diary"/>*/
+}
+{/*<img src="img/trouble.svg" alt="Diary"/>*/
+}
 const FeatureList = [
-  {
-    title: 'Powered by React',
-    Svg: require('../../static/img/CodeLogg.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: 'review',
+        Svg: require('../../static/img/diary.svg').default,
+        description: (
+            <>
+                what's in my TIL?
+            </>
+        ),
+        href: 'blog'
+    },
+    {
+        title: 'studyLog',
+        Svg: require('../../static/img/book.svg').default,
+        description: (
+            <>
+                in Progress
+            </>
+        ),
+        href: 'Study/OpenAPI/book/openAPI와스웨거를활용한실전API설계/01'
+
+    },
+    {
+        title: 'workLog',
+        Svg: require('../../static/img/rabbot.svg').default,
+        description: (
+            <>
+                daily work log
+            </>
+        ),
+        href: 'Daily/2024-03/daily/2024/03/18'
+    },
+    {
+        title: 'troubleShooting',
+        Svg: require('../../static/img/trouble.svg').default,
+        description: (
+            <>
+                Overcoming obstacles
+            </>
+        ),
+        href: 'Study/TroubleShooting/TroubleShooting/ElasticSearch연결안됨'
+    }
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({Svg, title, description, href}) {
+    return (
+        <div className={clsx('col col--3')} onClick={() => window.location.href = href}>
+            <div className={styles.featureBox}>
+                <Svg className={styles.featureSvg} alt={title}/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
